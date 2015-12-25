a75a98b5f29684a39af07dcdfe5efac084980506
Jeffrey de Wit <Nasgath@gmail.com>
2015-12-24 20:56:25 -0500

    Remove sidebars, don't need them just yet.

--- functions.php
+++ functions.php
@@ -92,24 +92,6 @@ function cheffism_content_width() {
 add_action( 'after_setup_theme', 'cheffism_content_width', 0 );
 
 /**
- * Register widget area.
- *
- * @link http://codex.wordpress.org/Function_Reference/register_sidebar
- */
-function cheffism_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'cheffism' ),
-		'id'            => 'sidebar-1',
-		'description'   => '',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
-}
-add_action( 'widgets_init', 'cheffism_widgets_init' );
-
-/**
  * Enqueue scripts and styles.
  */
 function cheffism_scripts() {
